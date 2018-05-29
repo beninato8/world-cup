2c8b3290a7366021b1bee0cab99b8475432130b2
beninato8 <beninato8@gmail.com>
2018-05-29 14:09:20 -0700

    py2app stuff

---
+++ players/make-new-player.app/Contents/Resources/a.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('touch ../../../new-player.txt')
---
+++ run-me.app/Contents/Resources/a.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('cd ../../../; ls; pwd; ./run-me.app/Contents/MacOS/python scoring.py')
+# os.system('cd ../../../; ls../MacOS/python ../../../scoring.py')
+# os.system('ls ../../../ > ~/Desktop/ls.txt')
