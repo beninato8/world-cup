8f5849159d21fcfcfcb854ac9f2ff994eef8fcc2
beninato8 <beninato8@gmail.com>
2018-05-30 01:03:30 -0700

    removed apps

--- run-me.app/Contents/Resources/a.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system('cd ../../../; ls; pwd; ./run-me.app/Contents/MacOS/python scoring.py')
-# os.system('cd ../../../; ls../MacOS/python ../../../scoring.py')
-# os.system('ls ../../../ > ~/Desktop/ls.txt')
